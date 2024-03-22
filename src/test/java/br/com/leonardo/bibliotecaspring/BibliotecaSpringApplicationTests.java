cff3506069fbccf66b600ee72f80772774740e0f
Leonardo Castilho <Leonardo_castilho1@hotmail.com>
2024-03-22 19:16:03 -0300

    Criação e primeiro commit do projeto

---
+++ src/test/java/br/com/leonardo/bibliotecaspring/BibliotecaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leonardo.bibliotecaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
