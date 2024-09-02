c214c9f541820211b93338c4a810323f0cc3bfd2
CastilhoLeo <leonardo_castilho1@hotmail.com>
2024-09-01 23:34:16 -0300

    Ajustes no projeto

--- src/main/java/br/com/leonardo/bibliotecaspring/dto/RegistroDTO.java
+++
@@ -1,6 +0,0 @@
-//package br.com.leonardo.bibliotecaspring.dto;
-//
-//import br.com.leonardo.bibliotecaspring.enums.Acesso;
-//
-//public record RegistroDTO(String login, String senha, Acesso acesso) {
-//}
